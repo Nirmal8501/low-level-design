58dfb7f4b8b9d3638e0087a7e8a64418aeb293ed
Nirmal Jadhao <nirmal.jadhao@gmail.com>
2026-03-03 23:08:05 +0530

    ParkingLot: Added parking floor and parking spot code

--- src/Problems/parkinglot/solution/model/ParkingFloor.java
+++ src/Problems/parkinglot/solution/model/ParkingFloor.java
@@ -1,10 +1,58 @@
 package Problems.parkinglot.solution.model;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ParkingFloor {
     private int floorNumber;
-    private Map<Integer, ParkingSpot> parkingSpots;
+    private Map<String, ParkingSpot> parkingSpots;
+
+//    displayInfo(), getAvailableParkingSpot(): ParkingSpot, addSpot
+
+    public ParkingFloor(int floorNumber) {
+        this.floorNumber = floorNumber;
+        parkingSpots = new HashMap<>();
+    }
+
+    public void addParkingSpot(ParkingSpot parkingSpot) {
+        parkingSpots.put(parkingSpot.getId(), parkingSpot);
+    }
+
+    public Optional<ParkingSpot> getAvailableParkingSpots(Vehicle vehicle) {
+        return parkingSpots.values()
+                .stream()
+                .filter(parkingSpot -> parkingSpot.isAvailable() && parkingSpot.canFitVehicle(vehicle))
+                .sorted(Comparator.comparing(ParkingSpot::getSpotSize)) // to get smallest possible size to fit the vehicle, but in our case we are mapping spot to vehicle size 1-1 so this wont do anything
+                .findFirst(); // or Findany()
+    }
+
+    public void displayInfo() {
+        System.out.println("Floor Number: " + floorNumber);
+        System.out.println("Available Parking Spots:");
+        parkingSpots.values()
+                .stream()
+                .peek(System.out::println);
+    }
+
+    public void displayAvailability() {
+        System.out.printf("--- Floor %d Availability ---\n", floorNumber);
+
+        Map<VehicleSize, Long> availableCounts = parkingSpots.values().stream()
+                .filter(spot -> !spot.getOccupied())
+                .collect(Collectors.groupingBy(ParkingSpot::getSpotSize, Collectors.counting()));
+
+        for (VehicleSize size : VehicleSize.values()) {
+            System.out.printf("  %s spots: %d\n", size, availableCounts.getOrDefault(size, 0L));
+        }
+    }
+
+
+    public int getFloorNumber(){
+        return floorNumber;
+    }
 
 
 }
--- src/Problems/parkinglot/solution/model/ParkingSpot.java
+++ src/Problems/parkinglot/solution/model/ParkingSpot.java
@@ -7,5 +7,70 @@ public class ParkingSpot {
     private Boolean isOccupied;
     private Vehicle parkedVehicle;
 
+    public ParkingSpot(String id, VehicleSize spotSize) {
+        this.id = id;
+        this.spotSize = spotSize;
+        this.isOccupied = false;
+        this.parkedVehicle = null;
+    }
 
+    public void parkVehicle(Vehicle vehicle) {
+        this.isOccupied = true;
+        this.parkedVehicle = vehicle;
+    }
+
+    public void unparkVehicle(Vehicle vehicle) {
+        this.isOccupied = false;
+        this.parkedVehicle = null;
+    }
+
+    public boolean canFitVehicle(Vehicle vehicle) {
+        return vehicle.getType().equals(this.spotSize); // You can have custom switch case here to accomodate different vehicles in same spot, i am keeping it simple
+    }
+
+    public boolean isAvailable() {
+        return !this.isOccupied;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public VehicleSize getSpotSize() {
+        return spotSize;
+    }
+
+    public void setSpotSize(VehicleSize spotSize) {
+        this.spotSize = spotSize;
+    }
+
+    public Boolean getOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(Boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public Vehicle getParkedVehicle() {
+        return parkedVehicle;
+    }
+
+    public void setParkedVehicle(Vehicle parkedVehicle) {
+        this.parkedVehicle = parkedVehicle;
+    }
+
+    @Override
+    public String toString() {
+        return "ParkingSpot{" +
+                "id='" + id + '\'' +
+                ", spotSize=" + spotSize +
+                ", isOccupied=" + isOccupied +
+                ", parkedVehicle=" + parkedVehicle +
+                '}';
+    }
 }
